b89cc73d6ba9d055f3903e1fe2f9d30b0df38562
Neil Murphy <barrymun@tcd.ie>
2025-05-14 20:10:43 +0100

    improving efficiency by only iterating over the hunks once, refactor HunkHeader component

--- src/components/patchViewer/hunkHeader/index.tsx
+++ src/components/patchViewer/hunkHeader/index.tsx
@@ -1,6 +1,4 @@
-import { TableRow, Typography } from "@mui/material";
-
-import MinimalTableCell from "../../styled/minimalTableCell";
+import { Box, Typography } from "@mui/material";
 
 interface HunkHeaderProps {
   children?: React.ReactNode;
@@ -8,12 +6,8 @@ interface HunkHeaderProps {
 
 export default function HunkHeader({ children }: HunkHeaderProps) {
   return (
-    <TableRow sx={{ bgcolor: "grey.50" }}>
-      <MinimalTableCell />
-      <MinimalTableCell />
-      <MinimalTableCell>
-        <Typography variant="body1" sx={{ color: "grey.500" }}>{children ?? "\u00a0"}</Typography>
-      </MinimalTableCell>
-    </TableRow>
+    <Box sx={{ bgcolor: "grey.50" }}>
+      <Typography variant="body1" sx={{ color: "grey.500" }}>{children ?? "\u00a0"}</Typography>
+    </Box>
   );
 }
