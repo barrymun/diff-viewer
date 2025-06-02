a30ed2652d4f7b56320302d0491d717cedcfe13e
Neil Murphy <barrymun@tcd.ie>
2025-06-02 18:53:42 +0100

    refactor left and right diff components into a single DiffSide component with a side prop

---
+++ src/components/diffViewer/sideBySideViewer/diffSide/index.tsx
@@ -0,0 +1,116 @@
+import { Box, TableRow, Typography, useTheme } from "@mui/material";
+import { diffWordsWithSpace, type Hunk } from "diff";
+
+import MinimalTableCell from "../../../styled/minimalTableCell";
+import { alignHunkLines } from "../../../../utils/helpers";
+
+interface DiffSideProps {
+  side: "left" | "right";
+  hunk: Hunk;
+}
+
+export default function DiffSide({ side, hunk }: DiffSideProps) {
+  const { spacing } = useTheme();
+  const alignedLines = alignHunkLines(hunk);
+
+  return alignedLines.map((line, idx) => {
+    const isAdded = line.oldLine === undefined && line.newLine !== undefined;
+    const isRemoved = line.newLine === undefined && line.oldLine !== undefined;
+    const isModified = line.oldLine && line.newLine && line.oldLine !== line.newLine;
+
+    const isLeft = side === "left";
+    const isRight = !isLeft;
+
+    const lineNumber = isLeft ? line.oldLineNumber : line.newLineNumber;
+    const lineContent = isLeft ? line.oldLine : line.newLine;
+
+    const bgColor = isLeft
+      ? isAdded
+        ? "grey.200"
+        : isRemoved || isModified
+        ? "red.50"
+        : "white"
+      : isRemoved
+      ? "grey.200"
+      : isAdded || isModified
+      ? "green.50"
+      : "white";
+
+    const symbol = isLeft
+      ? (isRemoved || isModified ? "-" : "")
+      : (isAdded || isModified ? "+" : "");
+
+    let renderedContent: React.ReactNode = lineContent ?? " ";
+
+    if (isModified) {
+      const diff = diffWordsWithSpace(line.oldLine!, line.newLine!);
+      renderedContent = diff
+        .filter(part => (isLeft ? !part.added : !part.removed))
+        .map((part, i) =>
+          (isLeft && part.removed) || (isRight && part.added) ? (
+            <Box
+              key={i}
+              component={isLeft ? "del" : "ins"}
+              sx={{
+                backgroundColor: isLeft ? "red.200" : "green.200",
+                display: "inline",
+              }}
+            >
+              {part.value}
+            </Box>
+          ) : (
+            <Box key={i} component="span" sx={{ display: "inline" }}>
+              {part.value}
+            </Box>
+          )
+        );
+    }
+
+    return (
+      <TableRow key={idx}>
+        <MinimalTableCell
+          sx={{
+            bgcolor: bgColor,
+            minWidth: spacing(7),
+            position: "sticky",
+            left: 0,
+            textAlign: "right",
+            userSelect: "none",
+          }}
+        >
+          <Typography
+            variant="body1"
+            component="span"
+            sx={{
+              color: "grey.500",
+              fontVariantNumeric: "tabular-nums",
+            }}
+          >
+            {lineNumber ?? ""}
+          </Typography>
+        </MinimalTableCell>
+        <MinimalTableCell
+          sx={{
+            bgcolor: bgColor,
+            minWidth: spacing(3),
+            textAlign: "center",
+            userSelect: "none",
+          }}
+        >
+          <Typography variant="body1" component="span">
+            {symbol}
+          </Typography>
+        </MinimalTableCell>
+        <MinimalTableCell sx={{ bgcolor: bgColor, width: "100%" }}>
+          <Typography
+            variant="body1"
+            component="span"
+            sx={{ whiteSpace: "pre", userSelect: "text" }}
+          >
+            {renderedContent}
+          </Typography>
+        </MinimalTableCell>
+      </TableRow>
+    );
+  });
+}
--- src/components/diffViewer/sideBySideViewer/index.tsx
+++ src/components/diffViewer/sideBySideViewer/index.tsx
@@ -1,8 +1,7 @@
 import { Box, Divider, Table, TableBody } from "@mui/material";
 import { type ParsedDiff } from "diff";
 
-import LeftSide from "./leftSide";
-import RightSide from "./rightSide";
+import DiffSide from "./diffSide";
 
 interface SideBySideViewerProps {
   parsedDiff: ParsedDiff;
@@ -15,7 +14,7 @@ export default function SideBySideViewer({ parsedDiff }: SideBySideViewerProps) 
       <Box sx={{ minWidth: 0, overflowX: "auto" }}>
         <Table>
           <TableBody>
-            {parsedDiff.hunks.map((hunk, hunkIndex) => <LeftSide key={hunkIndex} hunk={hunk} />)}
+            {parsedDiff.hunks.map((hunk, hunkIndex) => <DiffSide key={hunkIndex} side="left" hunk={hunk} />)}
           </TableBody>
         </Table>
       </Box>
@@ -25,7 +24,7 @@ export default function SideBySideViewer({ parsedDiff }: SideBySideViewerProps) 
       <Box sx={{ minWidth: 0, overflowX: "auto" }}>
         <Table>
           <TableBody>
-            {parsedDiff.hunks.map((hunk, hunkIndex) => <RightSide key={hunkIndex} hunk={hunk} />)}
+            {parsedDiff.hunks.map((hunk, hunkIndex) => <DiffSide key={hunkIndex} side="right" hunk={hunk} />)}
           </TableBody>
         </Table>
       </Box>
